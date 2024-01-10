7dc1b1be5454e5daa997767bebb14ee4a55b7858
zaid1baig <156165701+zaid1baig@users.noreply.github.com>
2024-01-10 18:57:40 +0530

    program_to_add_two_numbers

---
+++ addnumber.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main() {
+    int n;
+    printf("Enter a number\n");
+    scanf("%d", &n);
+    
+    if (n==1) {
+        printf("%d it is integer number\n", n);
+    } else {
+    	printf("is an charactera \n",n);
+       
+    }
+}
