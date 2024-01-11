c7737f7848b22b85725e75896f9de73fd7d4f60a
zaid1baig <156165701+zaid1baig@users.noreply.github.com>
2024-01-11 09:03:48 +0530

    less_then_greater_the_number

---
+++ condetion opreter2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a;
+	printf("enter a number\n");
+	scanf("%d",&a);
+	
+	if (a>10)
+	{
+		printf("%d is grater then 10",a);
+		
+	}
+	else
+	{
+		printf("%d a is less then 10",a);
+	}
+			
+	}
