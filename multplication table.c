dd26feb260fce4886e395b486e878992b83e3ca6
zaid1baig <156165701+zaid1baig@users.noreply.github.com>
2024-01-11 19:45:26 +0530

    Program to print multiplication table
    enter a number to print table10
    10*0=0
    10*1=10
    10*2=20
    10*3=30
    10*4=40
    10*5=50
    10*6=60
    10*7=70
    10*8=80
    10*9=90
    10*10=100

---
+++ multplication table.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int i,num;
+	printf("enter a number to print table");
+	scanf("%d",&num);
+    
+    for(i==1;i<=20;i++){
+    	printf("%d*%d=%d\n",num,i,(num*i));
+	}
+}
