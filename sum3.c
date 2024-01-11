745f1e975f6694247726b17d1899f7f3fa1f8d59
zaid1baig <156165701+zaid1baig@users.noreply.github.com>
2024-01-11 09:22:21 +0530

    sum_mat_div_rim

---
+++ sum3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+	int a,b;
+	printf("enter a to number\n");
+	scanf("%d%d",&a,&b);
+	int c=a+b;
+	printf("sum=%d\n",c);
+	c=a-b;
+		printf("sub=%d\n",c);
+	c=a*b;
+	    printf("mal=%d\n",c);
+	c=a/b;
+	    printf("div=%d\n",c);
+	c=a%b;
+	    printf("rim=%d\n",c);	    
+}
