23e5bff521ad66b4e990e9a01c483730c895a6c6
zaid1baig <156165701+zaid1baig@users.noreply.github.com>
2024-01-11 09:09:37 +0530

    using_if_else_see_to_numbers_equle_or_grater

---
+++ using if esle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b;
+	printf("enter first number\n");
+	scanf("%d%d",&a,&b);
+	
+	if (a>=b)
+	{
+		printf("%d a is grater then or equle to %d ",a,b);
+		
+	}
+	else
+	{
+		printf("%d a is less then %d",a,b);
+	}
+			
+	}
