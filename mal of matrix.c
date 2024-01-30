7b9b0469779844d2c23b84b115faf424c67b6b68
zaid1baig <156165701+zaid1baig@users.noreply.github.com>
2024-01-30 09:58:29 +0530

    multiplication_in_matrix_prg_no_12
    enter a value of matrix: 2
    enter a size of matrix: 2
    Enter elements in matrix A :
    1 2
    1 4
    
    Enter a matrix B :
    1 5
    2 8
    
    Sum of matrices
    1 10
    2 32
    
    --------------------------------
    Process exited after 19.5 seconds with return value 0
    Press any key to continue . . .

---
+++ mal of matrix.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+    int a[10][10],b[10][10],c[10][10],i,j,n,m;
+    printf("enter a value of matrix: ");
+    scanf("%d",&n);
+    printf("enter a size of matrix: ");
+    scanf("%d",&m);
+    printf("Enter elements in matrix A : \n");
+    
+    
+    
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<m; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    printf("\nEnter a matrix B : \n");
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<m; j++)
+        {
+            scanf("%d", &b[i][j]);
+        }
+    }
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<m; j++)
+        {
+            c[i][j] = a[i][j] * b[i][j];
+        }
+    }
+    printf("\nSum of matrices  \n");
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<m; j++)
+        {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+}
