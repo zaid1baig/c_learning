c92110758bf620d976e0c8dab79b59043c7c30f4
zaid1baig <156165701+zaid1baig@users.noreply.github.com>
2024-02-01 19:20:13 +0530

    Diagonal_matrix_program_13
    Enter matrix elements size :3
    enter a matrix element:
    1 2 3 4 5 6 7 8 9
    Matrix:
    1        2       3
    4        5       6
    7        8       9
    Main Diagonal: 1 5 9
    
    --------------------------------
    Process exited after 8.058 seconds with return value 0
    Press any key to continue . . .

---
+++ Diagonal matrix.1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+ {
+    int m[10][10],i,j,n;
+    printf("Enter matrix elements size :");
+    scanf("%d",&n);
+    printf("enter a matrix element:\n");
+    
+    for ( i = 0; i < n; i++) {
+        for ( j = 0; j < n; j++) {
+            scanf("%d", &m[i][j]);
+        }
+    }
+    printf("Matrix:\n");
+    for (i = 0; i < n; i++) {
+        for ( j = 0; j < n; j++) {
+            printf("%d\t ", m[i][j]);
+        }
+        printf("\n"); 
+    }
+    printf("Main Diagonal: ");
+    for ( i = 0; i < n; i++) {
+        printf("%d ", m[i][i]);
+    }
+    printf("\n");
+}
